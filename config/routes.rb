6accba5a739fb61887a31c56438b6dba9582a34f
Peter Johnson <Pjwr2313@gmail.com>
2019-04-01 22:02:00 +0000

    staticpages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources:users
 root 'application#home'
 end
-
